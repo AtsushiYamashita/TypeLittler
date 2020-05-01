1bb1a43161ee73e2c78367074659661688108c13
AtsushiYamashita <yays505@gmail.com>
2020-05-01 23:20:49 +0900

    Fix index declare

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import { PathValue } from "#/classes/PathValue"
 import { sum, append } from "#/functions/add"
 import { try_handle } from "#/functions/try_handle"
 
-export default {
+export const TypeLittler = {
     PathValue,
     sum,
     append,
     try_handle
 }
+
+export default TypeLittler
