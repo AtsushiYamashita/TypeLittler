8a4423c5c873a62d9b8e3d0e6997e311d9db317a
AtsushiYamashita <yays505@gmail.com>
2020-05-26 15:26:24 +0900

    Update try handler more native and symbol type

--- tests/functions/try_handle.test.ts
+++ tests/functions/try_handle.test.ts
@@ -6,7 +6,7 @@ const thrower = (n: number) => { if (n < 1) throw new Error("message"); return n
 test('try_handle@simple thrower', () => {
     const casef = try_handle(thrower, 100, 0);
     const casep = try_handle(thrower, 101, 1);
-    expect(casef[0]).toHaveLength(1);
+    expect(casef[0]).toHaveLength(2);
     expect(casef[1]).toHaveLength(0);
     expect(casep[0]).toHaveLength(0);
     expect(casep[1]).toHaveLength(1);
@@ -15,7 +15,7 @@ test('try_handle@simple thrower', () => {
 test('try_handle@simple thrower cb fail', () => {
     const casef = try_handle(thrower, 100, 0);
 
-    casef[0].forEach(err=>expect(err).toContain("100"))
+    expect(casef[0]).toContain(100)
     casef[1].forEach(res=>expect(res).toBe("unhappen"))
 });
 
